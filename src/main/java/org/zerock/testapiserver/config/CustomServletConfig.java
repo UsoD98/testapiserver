4347d0ee869ab45bb8b65f810c8e6abbf9f13ad1
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-05-13 10:52:34 +0900

    CORS

--- src/main/java/org/zerock/testapiserver/config/CustomServletConfig.java
+++ src/main/java/org/zerock/testapiserver/config/CustomServletConfig.java
@@ -3,6 +3,7 @@ package org.zerock.testapiserver.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.testapiserver.controller.formatter.LocalDateFormatter;
 
@@ -20,12 +21,12 @@ public class CustomServletConfig implements WebMvcConfigurer {
         registry.addFormatter(new LocalDateFormatter());
     }
 
-//    //CORS 설정 : Cross-Origin Resource Sharing (간단히 말하면 다른 도메인에서 요청을 허용하는 것) => security로 넘김
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .maxAge(500)
-//                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS")
-//                .allowedOrigins("*");
-//    }
+    //CORS 설정 : Cross-Origin Resource Sharing (간단히 말하면 다른 도메인에서 요청을 허용하는 것) => security로 넘김
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD","OPTIONS")
+                .allowedOrigins("*");
+    }
 }
